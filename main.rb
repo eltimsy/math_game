d1ec15c0a2b5bee84cac5ea022c723fc9373ec6f
eltimsy <timothy.sc.tse@gmail.com>
2016-10-07 20:55:57 +0000

    only one turn now

--- main.rb
+++ main.rb
@@ -8,38 +8,44 @@ player1 = MathGame::Player.new("Player1")
 player2 = MathGame::Player.new("Player2")
 question = MathGame::Questions.new("1")
 
+play = 1
+def playturn(play)
+  if play == 1
+    return player = 1
+  else
+    return player = 2
+  end
+end
+
 while player1.life > 0 || player2.life > 0 do
-  puts player1.name + " " + question.askquestion
+  player = playturn(play)
+  puts "player#{player} " + question.askquestion
   question.getanswer
   check = question.check
   if check == true
-    puts "#{player1.name} you are smart!"
+    puts "player#{player} you are smart!"
     puts "P1: #{player1.score} vs P2: #{player2.score}"
   else
-    puts "#{player1.name} you are dumb!"
-    player1.life -= 1
+    puts "player#{player} you are dumb!"
+    if player == 1
+      player1.life -= 1
+    else
+      player2.life -= 1
+    end
     puts "P1: #{player1.score} vs P2: #{player2.score}"
   end
   if player1.life < 1
     puts "#{player2.name} wins with a score of #{player2.score}"
     break
-  end
-  puts "----- NEW TURN -----"
-  puts player2.name + " " + question.askquestion
-  question.getanswer
-  check = question.check
-  if check == true
-    puts "#{player2.name} you are smart!"
-    puts "P1: #{player1.score} vs P2: #{player2.score}"
-  else
-    puts "#{player2.name} you are dumb!"
-    player2.life -= 1
-    puts "P1: #{player1.score} vs P2: #{player2.score}"
-  end
-  puts "----- NEW TURN -----"
-  if player2.life < 1
+  elsif player2.life < 1
     puts "#{player1.name} wins with a score of #{player1.score}"
     break
+  end
+  puts "----- NEW TURN -----"
+  if play == 1
+    play += 1
+  else
+    play -= 1
   end
 end
 puts "----- GAME OVER -----"
